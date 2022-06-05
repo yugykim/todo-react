667734115d63b4f1c6e5bf1db418e69eee1ebcdb
yugykim <yugyeongk92@gmail.com>
2022-06-04 22:17:02 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,64 +7,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { useForm } from "react-hook-form";
 
-const Globalstyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;1,200&display=swap');
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed, 
-  figure, figcaption, footer, header, hgroup, 
-  menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure, 
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
-  body {
-    line-height: 1;
-  }
-  ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    font-family: 'Source Sans Pro', sans-serif;
-    background-color: #dcdde1;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  `;
-
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -199,7 +141,6 @@ function App() {
 
   return (
     <>
-      <Globalstyle />
       <DragDropContext onDragEnd={onDragEnd}>
         <Wrapper>
           <ButtonForm onSubmit={handleSubmit(onSubmit)}>
